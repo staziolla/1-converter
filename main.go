9b17e10756baf7cdf61a29bcfb3c055f6eb6bd99
MaryLu <staziola@mail.ru>
2026-04-18 20:27:14 +0500

    1-start-second-commit

--- main.go
+++ main.go
@@ -6,4 +6,25 @@ func main() {
 	const cUSDtoRUB = 75.76
 	EURtoRUB := 1 / cUSDtoEUR * cUSDtoRUB
 	fmt.Println(EURtoRUB)
+
+	usum, ufrom, uto := getUserData()
+	calcResult(usum, ufrom, uto)
+}
+
+func getUserData() (float64, string, string) {
+	var usum float64
+	var ufrom, uto string
+	fmt.Print("Сумма: ")
+	fmt.Scan(&usum)
+	fmt.Print("из валюты : ")
+	fmt.Scan(&ufrom)
+	fmt.Print("в валюту : ")
+	fmt.Scan(&uto)
+	return usum, ufrom, uto
+
+}
+
+func calcResult(summa float64, fromVal string, toVal string) float64 {
+	fmt.Print(summa, fromVal, toVal)
+	return 0
 }
