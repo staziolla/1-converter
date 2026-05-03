e29034cd2e213deaf3ea78ee32a7c764a0ba5f48
MaryLu <staziola@mail.ru>
2026-05-04 01:11:52 +0500

    3struct third commit

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"time"
 	"fmt"
 	"test/project3/bins"
+	"test/project3/file"
+	"test/project3/storage"
+	"time"
 )
-
 
 func main() {
 	BinList := []bins.Bin{}
@@ -13,4 +14,13 @@ func main() {
 	BinList = append(BinList, *ptr)
 	BinList = append(BinList, *(bins.CreateBin("222", true, time.Now(), "dfgdfg")))
 	fmt.Println(BinList)
+
+	storage.SaveBinToFile("test1.json", BinList)
+
+	newdata, _ := storage.ReadBinFromFile("test1.json")
+	fmt.Println(newdata)
+
+	file.ReadAnyFile("test1.json")
+	fmt.Println(file.IsJsonFile("qqq.qqq"))
+	fmt.Println(file.IsJsonFile("test1.json"))
 }
