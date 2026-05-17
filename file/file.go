694adaaeafc1bfd5bb85ae51e49c49de7f3ab6a1
MaryLu <staziola@mail.ru>
2026-05-17 15:28:13 +0500

    4-interfaces 1

--- file/file.go
+++ file/file.go
@@ -6,6 +6,45 @@ import (
 	"strings"
 )
 
+type JsonDB struct {
+	filename string
+}
+
+func NewJsonDB(name string) *JsonDB {
+	return &JsonDB{filename: name,
+	}
+}
+
+func (db *JsonDB) Read() ([]byte, error) {
+	data, err := os.ReadFile(db.filename)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil, err
+	}
+	fmt.Println(string(data))
+	return data, nil
+}
+
+func (db *JsonDB) Write(content []byte) error {
+	file, err := os.Create(db.filename)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	defer file.Close()
+	_, err = file.Write(content)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	fmt.Println("Запись успешна")
+	return  nil
+}
+
+
+
+
+
 //Чтение любого файла
 //Проверка что это json расширение файла
 
@@ -24,37 +63,5 @@ func IsJsonFile(filename string) bool {
 }
 
 
-// type JsonDB struct {
-// 	filename string
-// }
 
-// func NewJsonDB(name string) *JsonDB {
-// 	return &JsonDB{filename: name,
-// 	}
-// }
-
-// func (db *JsonDB) Read() ([]byte, error) {
-// 	data, err := os.ReadFile(db.filename)
-// 	if err != nil {
-// 		outputs.PrintError(err)
-// 		return nil, err
-// 	}
-// 	fmt.Println(string(data))
-// 	return data, nil
-// }
-
-// func (db *JsonDB) Write(content []byte) {
-// 	file, err := os.Create(db.filename)
-// 	if err != nil {
-// 		outputs.PrintError(err)
-// 		return
-// 	}
-// 	defer file.Close()
-// 	_, err = file.Write(content)
-// 	if err != nil {
-// 		outputs.PrintError(err)
-// 		return
-// 	}
-// 	fmt.Println("Запись успешна")
-// }
 
